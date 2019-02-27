af69f7522a21e2e471abc5dd627f8ae57bf2f07c
Adam Patridge <patridge@gmail.com>
2019-02-26 21:41:19 -0700

    Unify whitespace.

--- Exercise 1/Start/MyAnimals/MyAnimals/Converters/ImageResourceConverter.cs
+++ Exercise 1/Start/MyAnimals/MyAnimals/Converters/ImageResourceConverter.cs
@@ -3,16 +3,16 @@ using Xamarin.Forms;
 
 namespace MyAnimals
 {
-	public class ImageResourceConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			return ImageSource.FromResource("MyAnimals.Images." + (value ?? ""), typeof(ImageResourceConverter));
+    public class ImageResourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ImageSource.FromResource("MyAnimals.Images." + (value ?? ""), typeof(ImageResourceConverter));
         }
 
-		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			throw new NotSupportedException();
-		}
-	}
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
